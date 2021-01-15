b1750480a442e45ca2dd9bf6e831c21057408674
LucasColas <lucas.colas13@gmail.com>
2021-01-15 21:13:44 +0100

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Gamma = 0.9
 
 def create_Q_table(env):
     ObservationSpace_Size = [30] * len(env.observation_space.low)
-    Q_table = [ObservationSpace_Size] 
+    Q_table = np.random.uniform(Low=-2, high=0, size=(ObservationSpace_Size + [env.action_space.n]))
     return Q_table
 
 
