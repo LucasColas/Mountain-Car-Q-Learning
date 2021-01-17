b4a33bc2e5963caa09b929aead4322c85fe59cee
LucasColas <lucas.colas13@gmail.com>
2021-01-17 09:24:00 +0100

    update

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import numpy as np
 
 env_name = "MountainCar-v0"
 env = gym.make(env_name)
+env.reset()
+obs_, reward,done, _ = env.step(1)
+print(obs_)
 #print( env.observation_space.high)
 #print(env.observation_space.low)
 
 Alpha = 0.1
 Gamma = 0.9
-Eps
+Eps = 1.0
 
 def create_Q_table():
-    
+    pass
